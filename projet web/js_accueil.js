c82115308175ace8401b83123cc996e6bec5599a
diispy <66866344+diispy@users.noreply.github.com>
2020-06-18 13:14:34 +0200

    Delete js_accueil.js

--- projet web/js_accueil.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function (){
-    $('.btn_menu').click(function(){
-        $(this).find('.barre').toggleClass('ouvert'); /* Changement de classe pour changer la couleur du bouton*/
-        $('.menu').toggleClass('ouvert'); /* Changement de classe pour ouvrir le menu*/
-    })
-});
